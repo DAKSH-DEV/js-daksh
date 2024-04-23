a7557ace12ab9897b32cd28f086d695379ecd160
DAKSH SHARMA <81735536+DAKSH-DEV@users.noreply.github.com>
2024-04-23 17:24:52 +0000

    datatypes

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "dakshsharma3758@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// accountID = 2 or somthing else not allowed //
+
+accountEmail = "daksh2134@gmail.com"
+
+/* prefer not to use var because of issue in block acope and functional scope */
+
+console.table([accountId , accountEmail, accountPassword , accountCity  ])
+
+tc
